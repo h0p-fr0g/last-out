5e33b379b762e601cc94aad855a6d7a22ac6fcb3
Merlin_ <ebubekirixde@gmail.com>
2026-07-06 13:42:52 +0200

    Door Animation

--- door.gd
+++ door.gd
@@ -1,135 +1,39 @@
 extends Node2D
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var detection_area: Area2D = $Area2D
-@onready var door_collision: CollisionShape2D = $StaticBody2D/CollisionShape2D
-@onready var door_open_sound: AudioStreamPlayer2D = $DoorOpenSound
-@onready var light_occluder: LightOccluder2D = $LightOccluder2D
-
-const ANIMATION_NAME: StringName = &"open_close"
-
-enum DoorState {
-	CLOSED,
-	OPENING,
-	OPEN,
-	CLOSING
-}
-
-var state: DoorState = DoorState.CLOSED
-var player_inside: bool = false
-
+@onready var anim_sprite = $AnimatedSprite2D
+@onready var collision_shape = $StaticBody2D/CollisionShape2D
+@onready var door_open_sound = $DoorOpenSound
+@onready var light_occluder = $LightOccluder2D
 
 func _ready() -> void:
-	animated_sprite.animation = ANIMATION_NAME
-	animated_sprite.frame = 0
-	animated_sprite.stop()
-	animated_sprite.speed_scale = 1.0
-
-	door_collision.set_deferred("disabled", false)
-
-	# Tür startet geschlossen, also blockiert sie Licht.
+	# 1. Wir zwingen die Tür beim Start, geschlossen zu sein
+	anim_sprite.animation = "open"
+	anim_sprite.frame = 0
+	anim_sprite.stop()
+	
+	# 2. Physik und Licht für geschlossene Tür aktivieren
+	collision_shape.set_deferred("disabled", false)
 	light_occluder.visible = true
 
-	detection_area.body_entered.connect(_on_body_entered)
-	detection_area.body_exited.connect(_on_body_exited)
-	animated_sprite.animation_finished.connect(_on_animation_finished)
-
-
-func _on_body_entered(body: Node) -> void:
-	if not body.is_in_group("player"):
-		return
-
-	player_inside = true
-	call_deferred("_request_open")
-
-
-func _on_body_exited(body: Node) -> void:
-	if not body.is_in_group("player"):
-		return
-
-	player_inside = false
-	call_deferred("_request_close")
-
-
-func _request_open() -> void:
-	if state == DoorState.OPEN or state == DoorState.OPENING:
-		return
-
-	_open_door()
-
-
-func _request_close() -> void:
-	if player_inside:
-		return
-
-	if state == DoorState.CLOSED or state == DoorState.CLOSING:
-		return
-
-	_close_door()
-
-
-func _open_door() -> void:
-	state = DoorState.OPENING
-
-	# Beim Öffnen blockiert die Tür nicht mehr.
-	door_collision.set_deferred("disabled", true)
-	light_occluder.visible = false
-
-	_play_door_sound()
-
-	animated_sprite.speed_scale = 1.0
-	animated_sprite.play(ANIMATION_NAME)
-
-
-func _close_door() -> void:
-	state = DoorState.CLOSING
-
-	# Beim Schließen soll die Tür wieder blockieren.
-	door_collision.set_deferred("disabled", false)
-	light_occluder.visible = true
-
-	var last_frame := animated_sprite.sprite_frames.get_frame_count(ANIMATION_NAME) - 1
-
-	if animated_sprite.frame < last_frame:
-		animated_sprite.frame = last_frame
-
-	_play_door_sound()
-
-	animated_sprite.speed_scale = -1.0
-	animated_sprite.play(ANIMATION_NAME)
-
-
-func _play_door_sound() -> void:
-	if door_open_sound == null:
-		return
-
-	door_open_sound.stop()
-	door_open_sound.play()
-
-
-func _on_animation_finished() -> void:
-	if animated_sprite.speed_scale > 0.0:
-		state = DoorState.OPEN
-
-		var last_frame := animated_sprite.sprite_frames.get_frame_count(ANIMATION_NAME) - 1
-		animated_sprite.frame = last_frame
-		animated_sprite.stop()
-
-		door_collision.set_deferred("disabled", true)
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		# Tür geht auf: Kollision und Schatten aus, Animation vorwärts abspielen
+		collision_shape.set_deferred("disabled", true)
 		light_occluder.visible = false
+		
+		if door_open_sound:
+			door_open_sound.play()
+			
+		anim_sprite.play("open")
 
-		if not player_inside:
-			call_deferred("_request_close")
-
-	else:
-		state = DoorState.CLOSED
-
-		animated_sprite.frame = 0
-		animated_sprite.stop()
-		animated_sprite.speed_scale = 1.0
-
-		door_collision.set_deferred("disabled", false)
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		# Tür geht zu: Kollision und Schatten wieder an, Animation rückwärts abspielen
+		collision_shape.set_deferred("disabled", false)
 		light_occluder.visible = true
-
-		if player_inside:
-			call_deferred("_request_open")
+		
+		if door_open_sound:
+			door_open_sound.play()
+			
+		# Das ist der Zauberbefehl: Er spielt die Animation wie ein Videoband rückwärts ab
+		anim_sprite.play_backwards("open")
